a0da788ae42f5c107815d700ebe0f8ed3e9cd53b
bagasstr <bagassatrio2805@gmail.com>
2024-08-01 21:01:47 +0700

    Create a brief projects from devchallenges.io

--- src/components/molecules/TabsItems.jsx
+++ src/components/molecules/TabsItems.jsx
@@ -4,24 +4,23 @@ import { useTabs } from "../../utils/context/TabActive";
 import { useState, useEffect } from "react";
 import Card from "../atoms/Card";
 
-export default function TabsItems({ label, children }) {
+export default function TabsItems() {
   const { isActive } = useTabs();
   const [filteredData, setFilteredData] = useState({
     available: [],
     allProduct: [],
   });
-  const tabs = ["All Products", "Available Now"];
 
   useEffect(() => {
     fetchData();
   }, []);
+
   const fetchData = async () => {
     try {
       const res = await fetch(
         "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json"
       );
       const data = await res.json();
-      // console.log(data);
       setFilteredData({
         available: data.filter((item) => item.available === true),
         allProduct: data,
